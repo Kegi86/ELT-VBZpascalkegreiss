9393b6964fbfe8c2c26ab5cdd763455edf6f3a20
Pascal <pascal.kegreiss@edu.teko.ch>
2020-10-04 16:28:42 +0200

    scripts push

--- Scripts/Aufgabe_11_visualisierung_linie.sql
+++ Scripts/Aufgabe_11_visualisierung_linie.sql
@@ -1,15 +1,17 @@
 select distinct 
 	h.GPS_Latitude as lat,
     h.GPS_Longitude as lng,
-   	null as color,
-    h2.halt_lang as note
+    h2.halt_lang as name,
+    null as color,
+    null as note
 from
     vbzdat.haltepunkt h
 inner join vbzdat.ankunftszeiten a on
     h.halt_punkt_id = a.haltepunkt_id
 inner join vbzdat.haltestelle h2 on
     h.halt_id = h2.halt_id
-
 group by h2.halt_lang;
    
 
+
+ 	
