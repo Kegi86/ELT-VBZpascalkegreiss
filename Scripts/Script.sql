2fa82af86ac1481fa72ec735d14daaad88c143f3
Pascal <pascal.kegreiss@edu.teko.ch>
2020-10-04 22:19:24 +0200

    wirklich final push ;)

--- Scripts/Script.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE vbzdat.haltepunkt MODIFY COLUMN halt_punkt_id int NOT NULL;
-ALTER TABLE vbzdat.haltepunkt ADD CONSTRAINT haltepunkt_pk PRIMARY KEY (halt_punkt_id);
-
-
-ALTER TABLE vbzdat.haltestelle MODIFY COLUMN halt_id int NOT NULL;
-ALTER TABLE vbzdat.haltestelle ADD CONSTRAINT haltestelle_pk PRIMARY KEY (halt_id);
